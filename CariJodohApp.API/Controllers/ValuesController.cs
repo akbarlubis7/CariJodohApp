a4f92a6d4884e7596fde5b6894da896288b57ed6
Akbar Syahputra Lubis <akbar.s@lt-indonesia.com>
2020-06-09 11:23:35 +0700

    End of section 3

--- CariJodohApp.API/Controllers/ValuesController.cs
+++ CariJodohApp.API/Controllers/ValuesController.cs
@@ -2,11 +2,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CariJodohApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace CariJodohApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -27,7 +29,7 @@ namespace CariJodohApp.API.Controllers
             return Ok(values);
         }
 
-        //GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
--- CariJodohApp.API/Data/DataContext.cs
+++ CariJodohApp.API/Data/DataContext.cs
@@ -9,5 +9,6 @@ namespace CariJodohApp.API.Data
         (options){}
 
         public DbSet<Value> Values {get; set;}
+        public DbSet<User> Users {get; set;}
     }
 }
