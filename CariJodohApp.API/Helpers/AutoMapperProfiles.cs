4d76b19dfa3065b712191427d2ac9dc030b75916
Akbar Syahputra Lubis <akbar.s@lt-indonesia.com>
2020-06-15 22:57:22 +0700

    End of section 8

---
+++ CariJodohApp.API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using AutoMapper;
+using CariJodohApp.API.Dto;
+using CariJodohApp.API.Models;
+
+namespace CariJodohApp.API.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, UserForListDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<User, UserForDetailDto>()
+                .ForMember(dest => dest.PhotoUrl, 
+                    opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
+            CreateMap<Photo, PhotosForDetailDto>();
+        }
+    }
+}
